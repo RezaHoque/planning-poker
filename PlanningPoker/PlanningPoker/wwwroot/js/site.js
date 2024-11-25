bb842ac42e251f375c33356760a26f7738607593
rezaul hoque <mo.reza.hoque@gmail.com>
2024-11-25 22:47:46 +0100

    leave, join room button function.

--- PlanningPoker/PlanningPoker/wwwroot/js/site.js
+++ PlanningPoker/PlanningPoker/wwwroot/js/site.js
@@ -1,1 +1,57 @@
 ﻿
+document.addEventListener("DOMContentLoaded", function () {
+
+    const leaveButton = document.getElementById("btnLeave");
+    const joinRoomBtn = document.getElementById("joinRoomBtn");
+    const navUserName = document.getElementById("navUserName");
+    const returnUrl = getQueryStringParameter("returnUrl");
+
+
+    if (!getCookie("userName")) {
+        leaveButton.style.display = "none";
+    } else {
+        navUserName.textContent = getCookie("userName");
+    }
+   
+    if (joinRoomBtn) {
+        if (returnUrl) {
+            joinRoomBtn.textContent = "Join room";
+        } else {
+            joinRoomBtn.textContent = "Start a room";
+        }
+        joinRoomBtn.addEventListener("click", function () {
+            const userName = document.getElementById("userName").value;
+            const roomId = document.getElementById("roomId").value;
+
+            if (!userName) {
+                alert("Please enter your name!");
+                return;
+            }
+            setCookie("userName", userName, 1);
+
+            window.location.href = returnUrl ? returnUrl : `/room/${roomId}`;
+        });
+    }
+    
+    leaveButton.addEventListener("click", function () {
+        document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        window.location.href = "/";
+    });
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return null;
+    } 
+
+    function getQueryStringParameter(name) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(name);
+    }
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000); // Days to milliseconds
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = `${name}=${value}; ${expires}; path=/`;
+    }
+});
